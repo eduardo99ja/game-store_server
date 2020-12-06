1d5d0bf405aa2439cbbf7e97ff31c8f892130fb1
Eduardo Apodaca <eduardo1ja99@gmail.com>
2020-12-05 23:23:44 -0600

    iniciando proyecto con strapi

---
+++ config/database.js
@@ -0,0 +1,20 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        host: env('DATABASE_HOST', 'cluster0.a4kcu.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'ecommerce'),
+        username: env('DATABASE_USERNAME', 'apodacaDEV'),
+        password: env('DATABASE_PASSWORD', '4keUh6tcWLGbCfJr'),
+      },
+      options: {
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
+      },
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '74a13c61-7231-47b8-bb91-9a6378a592ff'
+};
